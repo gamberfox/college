486c36021a369b857d2bf708f4f8fe2ff321255d
unknown <gamberfox@gmail.com>
2023-04-23 06:18:44 -0500

    bo

--- DB/t2/4.sql
+++ DB/t2/4.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS cliente CASCADE;
 CREATE TABLE cliente
 (
-cedula VARCHAR(15) NOT NULL PRIMARY KEY,
+cedula VARCHAR(10) NOT NULL PRIMARY KEY,
 nombre VARCHAR(50) NOT NULL,
 direccion VARCHAR(50),
 telefono VARCHAR(10)
@@ -39,8 +39,8 @@ CREATE TABLE proyeccion
 );
 
 
-DROP TABLE IF EXISTS peliculas_cliente CASCADE;
-CREATE TABLE peliculas_cliente
+DROP TABLE IF EXISTS pelicula_cliente CASCADE;
+CREATE TABLE pelicula_cliente
 (
   codigo_pelicula VARCHAR(10) NOT NULL,
   cedula VARCHAR(15) NOT NULL,
@@ -51,17 +51,17 @@ CREATE TABLE peliculas_cliente
 
 -- comandos para agregar cosas
 
-INSERT INTO cliente(cedula,nombre,direccion,telefono) VALUES('123456','jon','calle 6 3-23','1123456')
-INSERT INTO cliente VALUES('123457','mike','calle 7 32-34','112347')
-INSERT INTO cliente VALUES('123458','mike','calle 8 3-35','112348')
-INSERT INTO cliente VALUES('123459','mike','calle 9 3-36','112349')
-INSERT INTO cliente VALUES('123450','mike','calle 0 3-37','112340')
+INSERT INTO cliente(cedula,nombre,direccion,telefono) VALUES('1111111111','jon','calle 6 3-23','1123456')
+INSERT INTO cliente VALUES('1111111112','mike','calle 7 32-34','112347')
+INSERT INTO cliente VALUES('1111111113','mike','calle 8 3-35','112348')
+INSERT INTO cliente VALUES('1111111114','mike','calle 9 3-36','112349')
+INSERT INTO cliente VALUES('1111111115','mike','calle 0 3-37','112340')
 
 INSERT INTO sala(numero_sala,piso,capacidad) VALUES('1','1',30)
-INSERT INTO sala VALUES('1','1',30)
 INSERT INTO sala VALUES('2','1',30)
-INSERT INTO sala VALUES('3','2',20)
+INSERT INTO sala VALUES('3','1',30)
 INSERT INTO sala VALUES('4','2',20)
+INSERT INTO sala VALUES('5','2',20)
 
 INSERT INTO pelicula(codigo_pelicula,titulo,genero,director) VALUES('1234','stomp','comedy','jason')
 INSERT INTO pelicula VALUES('1235','mad','drama','quentin')
@@ -70,12 +70,14 @@ INSERT INTO pelicula VALUES('1237','instance','drama','summer')
 INSERT INTO pelicula VALUES('1238','godless','fantasy','yamamoto')
 
 INSERT INTO proyeccion (numero_sala,codigo_pelicula,fecha,hora) VALUES('1','1234','14/04/2023','13:00') 
-INSERT INTO proyeccion VALUES('1','1234','14/04/2023','13:00')
-INSERT INTO proyeccion VALUES('1','1234','14/04/2023','13:00')
-INSERT INTO proyeccion VALUES('1','1234','14/04/2023','13:00')
-INSERT INTO proyeccion VALUES('1','1234','14/04/2023','13:00')
+INSERT INTO proyeccion VALUES('2','1235','14/04/2023','13:00')
+INSERT INTO proyeccion VALUES('3','1236','14/04/2023','13:00')
+INSERT INTO proyeccion VALUES('4','1237','14/04/2023','13:00')
+INSERT INTO proyeccion VALUES('5','1238','14/04/2023','13:00')
 
- numero_sala VARCHAR(4) NOT NULL,
-  codigo_pelicula VARCHAR(10) NOT NULL,
-  fecha VARCHAR(15),
-  hora VARCHAR(10),
+
+INSERT INTO pelicula_cliente(codigo_pelicula,cedula,categoria) VALUES('1234','1111111111','todos')
+INSERT INTO pelicula_cliente VALUES('1235','1111111112','adultos')
+INSERT INTO pelicula_cliente VALUES('1236','1111111113','adolescentes')
+INSERT INTO pelicula_cliente VALUES('1237','1111111114','adultos')
+INSERT INTO pelicula_cliente VALUES('1238','1111111115','adultos')
